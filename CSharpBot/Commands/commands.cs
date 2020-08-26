60f3c29b0507150da644199c248314d07db1df37
Kira <54922675+Siri-chan@users.noreply.github.com>
2020-08-26 10:01:10 +1000

    added qgamer command
    poggers

--- CSharpBot/Commands/commands.cs
+++ CSharpBot/Commands/commands.cs
@@ -19,18 +19,23 @@ namespace CSharpBot.Commands
         [Description("Adds 2 Numbers Together")]
         public async Task AddNumber(CommandContext ctx, [Description("The Number that you are adding to numberTwo.")] int numberOne, [Description("The Number that you are adding to numberOne.")] int numberTwo)
         {
-            await ctx.Channel.SendMessageAsync((numberOne).ToString() + "+" + (numberTwo).ToString() + "=" + (numberOne+numberTwo).ToString()).ConfigureAwait(false);
+            await ctx.Channel.SendMessageAsync((numberOne).ToString() + "+" + (numberTwo).ToString() + "=" + (numberOne + numberTwo).ToString()).ConfigureAwait(false);
         }
         [Command("roll")]
         [Description("Rolls a Random Integer between 1 and `max`")]
         public async Task Roll(CommandContext ctx, [Description("The maximum number you can acheive by rolling.")] int max)
         {
             Random rnd = new Random();
-                int diceRoll = 0;
-                diceRoll = rnd.Next(1,max + 1);
+            int diceRoll = 0;
+            diceRoll = rnd.Next(1, max + 1);
             await ctx.Channel.SendMessageAsync((diceRoll).ToString()).ConfigureAwait(false);
         }
+        [Command("gamer")]
+        [Description("[CONTENT REDACTED]")]
+        public async Task gamer(CommandContext ctx)
+        {
+            await ctx.Member.SendMessageAsync("https://drive.google.com/drive/folders/1wf-q6U0_k6IDGD3nEqVQQOOyUZ9GtPtp").ConfigureAwait(false);
+        }
     }
-
 }
 
