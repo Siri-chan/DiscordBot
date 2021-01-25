1d9e8932aa8bfb8c70c1d04b242aef38580a7ea0
Kira <54922675+Siri-chan@users.noreply.github.com>
2021-01-26 04:20:39 +0900

    Added FizzBuzz Command and Finished README

--- CSharpBot/Commands/commands.cs
+++ CSharpBot/Commands/commands.cs
@@ -15,7 +15,7 @@ namespace CSharpBot.Commands
         [Description("returns information about the bot.")]
         public async Task Credits(CommandContext ctx, [Description("1: General Credits; 2: Source Code")] int page)
         {
-            string[] content = { "Written in C# using the DSharpPlus Libraries, by Siri.", "Source Code available at https://github.com/Siri-chan/DiscordBotTest (link may be subject to change)" };
+            string[] content = { "Written in C# using the DSharpPlus Libraries, by Siri.", "Source Code available at https://github.com/Siri-chan/DiscordBot (link may be subject to change)" };
             if (page <= content.Length + 1 & page > 0)
             {
                 await ctx.Channel.SendMessageAsync(content[page - 1]).ConfigureAwait(false);
@@ -54,15 +54,15 @@ namespace CSharpBot.Commands
             if (not492 % 492 == 0)
             {
                 int div492 = not492 / 492;
-                if (div492 > 2)
+                if (div492 % 10 > 2)
                 {
                     await ctx.Channel.SendMessageAsync(not492.ToString() + " is the " + div492.ToString() + "th multiple of 492.").ConfigureAwait(false);
                 }
-                else if (div492 == 2)
+                else if (div492 % 10 == 2)
                 {
                     await ctx.Channel.SendMessageAsync(not492.ToString() + " is the " + div492.ToString() + "nd multiple of 492.").ConfigureAwait(false);
                 }
-                else if (div492 == 1)
+                else if (div492 % 10 == 1)
                 {
                     await ctx.Channel.SendMessageAsync(not492.ToString() + " is the " + div492.ToString() + "st multiple of 492.").ConfigureAwait(false);
                 }
@@ -70,6 +70,10 @@ namespace CSharpBot.Commands
                 {
                     await ctx.Channel.SendMessageAsync("i can't believe you didn't think i would add this stupid gag in here for if you were a smartass and put zero in").ConfigureAwait(false);
                 }
+            }
+            else
+            {
+                await ctx.Channel.SendMessageAsync("This number is not divisible by 492, and it leaves a remainder of " + not492 % 492).ConfigureAwait(false);
             }
         }
         /*        [Command("timezone")]
@@ -108,6 +112,87 @@ namespace CSharpBot.Commands
             await ctx.Channel.SendMessageAsync("{awaiting reaction}").ConfigureAwait(false);
             var message = await interactivity.WaitForReactionAsync(x => x.Channel == ctx.Channel && x.User == ctx.User).ConfigureAwait(false);
             await ctx.Channel.SendMessageAsync(message.Result.Emoji).ConfigureAwait(false);
+        }
+        [Command("fizzbuzz")]
+        [Description("Play the simple game of fizzbuzz.")]
+        public async Task FizzBuzz(CommandContext ctx, [Description("Determines how long the game goes on for.")] int length)
+        {
+            // This game is still a little broken, it will treat every user's response as an entry to the game, and will occasionally recieve the same message twice (discord's servers sometimes recieve msgs multiple times)
+            // It will also count any input as an answer to the game, which can be fixed by a simple if(){} that checks the message content is an integer or fizz/buzz/fizzbuzz , but i'm too lazy to glue it in for now, because it will inevitably fail in some way
+            bool winning = true;
+            int fizz = 3;
+            int buzz = 5;
+            var interactivity = ctx.Client.GetInteractivity();
+            await ctx.Channel.SendMessageAsync("Count to " + length + ". You must replace " + fizz + "s with 'fizz' and " + buzz +"s with 'buzz'. Numbers that match both should be answered with 'fizzbuzz'.").ConfigureAwait(false);
+            for (int i = 1; i <= length; i++)
+            {
+                var message = await interactivity.WaitForMessageAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
+                if (i % fizz == 0)
+                {
+                    if (i % buzz == 0)
+                    {
+                        if (message.Result.Content == "fizzbuzz")
+                        {
+                            await ctx.Channel.SendMessageAsync("good").ConfigureAwait(false);
+                        }
+                        else
+                        {
+                            await ctx.Channel.SendMessageAsync("bad, correct was fizzbuzz").ConfigureAwait(false);
+                            winning = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (message.Result.Content == "fizz")
+                        {
+                            await ctx.Channel.SendMessageAsync("good").ConfigureAwait(false);
+                        }
+                        else
+                        {
+                            await ctx.Channel.SendMessageAsync("bad, correct was fizz").ConfigureAwait(false);
+                            winning = false;
+
+                            break;
+                        }
+                    }
+                }
+                else if (i % buzz == 0)
+                {
+                    if (message.Result.Content == "buzz")
+                    {
+                        await ctx.Channel.SendMessageAsync("good").ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await ctx.Channel.SendMessageAsync("bad, correct was buzz").ConfigureAwait(false);
+                        winning = false;
+                        break;
+
+                    }
+                }
+                else
+                {
+                    if (message.Result.Content == i.ToString())
+                    {
+                        await ctx.Channel.SendMessageAsync("good").ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await ctx.Channel.SendMessageAsync("bad, correct was " + i).ConfigureAwait(false);
+                        winning = false;
+                        break;
+                    }
+                }
+            }
+            if (winning == true)
+            {
+                await ctx.Channel.SendMessageAsync("Poggers, You Win!").ConfigureAwait(false);
+            }
+            else
+            {
+                await ctx.Channel.SendMessageAsync("KEKW, You lost a simple kid's game!").ConfigureAwait(false);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -8,33 +8,77 @@ For this to work you need to create a file titled config.json in `/bin/Debug/net
 	"prefix": "<insert here>",
 }
 ```
-Then, compile it with Microsoft .NET.
+Then, compile it with Microsoft .NET.  
 ## Commands
 ### qcredits
 **Syntax:** `qcredits <page>`  
 **Conditions:** Requires an Integer to take the place of the `page` variable. Page 1 = General Credits; Page 2 = Source Code  
 **Description:** Provides information about the bot. 
-**Class:** Credits  
+**Class:** `Credits`  
 **Example:** Input: `qcredits 1`  
-		Output: `Written in C# using the DSharpPlus Libraries, by Siri.`
+			Output: `Written in C# using the DSharpPlus Libraries, by Siri.`
 ### qadd
 **Syntax:** `qadd <numberOne> <numberTwo>`  
 **Conditions:** Requires an Integer to take the place of the `numberOne` and `numberTwo` variable.   
 **Description:** Outputs a simple equation.  
-**Class:** AddNumber  
+**Class:** `AddNumber`  
 **Example:** Input: `qadd 5 10`  
-		Output: `5+10=15`   
+			Output: `5+10=15`   
 		  
 ### qroll
 **Syntax:** `qroll <max>`  
 **Conditions:** Requires an Integer to take the place of the `max` variable.   
 **Description:** Generates a Random Number between 1 and `max`.    
-**Class:** Roll  
+**Class:** `Roll`  
 **Example:** Input: `qroll 20`  
-		Output: `15`   
+			Output: `15`   
 		
 ### qgamer  
 **[CONTENT REDACTED]**
-		  
 
-		  
+### q492  
+**Syntax:** `q492 <not492>`  
+**Conditions:** Requires a positive integer to replace the `not492` variable.  
+**Description:** This one's quite self-explanatory.  
+**Class:** `fourHundredAndNinetyTwo`  
+**Example:** Input: `q492 984`  
+			Output: `984 is the 2nd multiple of 492.`  
+
+_qtimezone is an unfinished command, fully commented out in the code_
+
+### qrepeat
+**Syntax:** `qrepeat`  
+**Conditions:** Requires another message to be sent after the command.  
+**Description:** Repeats the channel's next message back.  
+**Class:** `repeat`
+**Example:** Input: `qrepeat`  
+			Output: `{awaiting message}`  
+			Input: `haha gay`  
+			Output: `haha gay`  
+
+###  qrepeatemoji
+**Syntax:** `qrepeatemoji`  
+**Conditions:** Requires a message to be reacted to after the command.  
+**Description:** Repeats the channel's next emoji back.  
+**Class:** `repeatEmoji`
+**Example:** Input: `qrepeatemoji`  
+			Output: `{awaiting reaction}`  
+			Input: _User reacts to the bot's message with :ok_hand:`_  
+			Output: `:ok_hand:`  
+
+### qfizzbuzz  
+**Syntax:** `qfizzbuzz <length>`  
+**Conditions:** Requires a positive integer to take the place of the `length` variable. You must also continue to play the game after sending the command.  
+**Description:** Play the simple game of fizzbuzz.  
+**Class:** `FizzBuzz`  
+**Example:** Input: `qfizzbuzz 100`  
+			Output: `Count to 100. You must replace 3s with 'fizz' and 5s with 'buzz'. Numbers that match both should be answered with 'fizzbuzz'.  
+			_user plays the game lol_  
+
+
+
+## Distribution
+Feel free to distribute/build the bot in whatever config and with whatever alterations you wish, just make sure to credit every previous author.
+
+## Bugs and Issues
+Let's be honest. I make a lot of dumb mistakes. If you find one, feel free to use the issues page at https://github.com/Siri-chan/DiscordBot to report bugs, or create your own branch to fix them.
